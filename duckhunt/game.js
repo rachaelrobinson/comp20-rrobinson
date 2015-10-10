df36bcb5904be436099a8e03875eeb01914bb4ea
Rachael Robinson <Rachael@Rachaels-MacBook-Pro.local>
2015-10-10 19:06:47 -0400

    drew background on canvas

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,9 @@
-// Replace this with your code...
+function init(){
+	var canvas = document.getElementById('game_canvas');
+	var ctx = canvas.getContext('2d');
+	var img = new Image();
+	img.onload = function(e){
+		ctx.drawImage(img, 0,0);
+	};
+	img.src = './duckhunt-background.gif';
+};
