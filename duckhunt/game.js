951f29b8ed8362602dad15bb18836822a66a0b2a
Rachael Robinson <Rachael@resnet157-57.medford.tufts.edu>
2015-10-19 22:45:59 -0400

    commit to rebase/merge

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,29 +1,40 @@
+var timer = 0
 function init(){
 	var canvas = document.getElementById('game_canvas');
 	var ctx = canvas.getContext('2d');
 	var img = new Image();
 	img.onload = function(){
 		ctx.drawImage(img,0,0);
+		timer = 0;
 	};
 	img.src = './duckhunt-background.gif';
-	drawBird1(ctx);
-	drawBird2(ctx);
+	setInterval(drawBird1,500, ctx, timer);
 };
 
-function drawBird1(ctx){
+function drawBird1(ctx, timer){
 	var bird1 = new Image();
-	bird1.onload = function(){
-		ctx.drawImage(bird1,0,118,37,37,50,50,45,45);
-	};
+	console.log(timer);
+	if (timer == 0) {
+		bird1.onload = function(){
+			ctx.drawImage(bird1,0,118,37,37,50,50,45,45);
+			timer = 1;
+		};
+	} else {
+		bird1.onload = function(){
+			ctx.drawImage(bird1,38,118,37,37,50,50,45,45);
+			timer = 0;
+		};
+
+	}
 	bird1.src = './duckhunt_various_sheet.png'
-
+	console.log("timer is", timer);
 };
 
-function drawBird2(ctx){
-	var bird2 = new Image();
-	bird2.onload = function(){
-		ctx.drawImage(bird2,124,114,37,37,70,70,45,45);
-	};
+// function drawBird2(ctx){
+// 	var bird2 = new Image();
+// 	bird2.onload = function(){
+// 		ctx.drawImage(bird2,124,114,37,37,70,70,45,45);
+// 	};
 
-	bird2.src = './duckhunt_various_sheet.png'
-};
+// 	bird2.src = './duckhunt_various_sheet.png'
+// };
