97c6aa8ef3e090af58e8f5918b95e9c7e3de2a68
Rachael Robinson <Rachael@resnet157-57.medford.tufts.edu>
2015-10-19 22:41:08 -0400

    data.json parsed and messages outputted in messages section of HTML and css implemented

--- messages/lab.js
+++ messages/lab.js
@@ -1,3 +1,21 @@
 function parse(){
-	var http = new XMLHttpRequest();	
+	var http = new XMLHttpRequest();
+	http.open("GET", "data.json", true);
+	// console.log(http);
+	http.onreadystatechange = function(){
+		if(http.readyState === 4) {
+			
+			addMessage(http.responseText);
+		}
+	};	
+	http.send();
+};
+
+function addMessage(JSONdata) {
+	var parsedObj = JSON.parse(JSONdata);
+	console.log("PARSED = ", parsedObj);
+	var messages = document.getElementById('messages');
+	for (var i = 0; i < parsedObj.length; i++) {
+		messages.innerHTML += "<p>" + parsedObj[i]["content"] + " " + "<span>" + parsedObj[i]["username"] + "</span" + "</p>";
+	}
 };
